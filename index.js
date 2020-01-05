8200d2932f7ee5f8da99606e0436a37d87d57561
Matthew Taylor <matt@mtaylor.tech>
2020-01-04 19:28:49 -0500

    complete

--- index.js
+++ index.js
@@ -303,8 +303,15 @@ const argTimesTwo = (num) => { num*2 }; // code here!
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
 */
-function carMaker(/* code here */) {
-  /* code here */
+function carMaker(odo_val) {
+  const car = {
+    odometer: odo_val,
+    drive: function(odo_plus) {
+        this.odometer += odo_plus;
+        return this.odometer;
+    },
+  };
+  return car;
 }
 
 /// ////// END OF CHALLENGE /////////
